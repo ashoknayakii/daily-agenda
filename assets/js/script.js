f8afb3805ae6f7cf16ddc2b09636c4011217a719
Ashok Nayak <ashok.nayakii@gmail.com>
2022-01-28 14:51:19 -0500

    color coding conditional statements added

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,20 +8,36 @@ var todayDate = moment().format('MMMM Do, YYYY');
 $("#currentDay").append(todayDate);
 
 
-
-
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours
-
-
-
-
-
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+function colorCode () {
+    var currentHour = moment().hours();
+    console.log ('currentHour:' , currentHour);
+    
+    $(".time-block").each(function(){
+        var hourBlock = parseInt(
+            $(this).attr("id")
+        );
+        console.log('hourBlock:' , hourBlock)
+            if (hourBlock < currentHour){
+            $(this).addClass('past')}
+            else if (hourBlock === currentHour){
+            $(this).addClass('present')}
+            else {$(this).addClass('future')}
+        })
+        console.log('colorcode checked')
+    }
 
-
-
+$(document).ready(function(){
+   
+        
+        
+    colorCode();
+    var checkTime = setInterval(() => {
+    colorCode}, 30);
+})
 
 
 // WHEN I click into a time block
